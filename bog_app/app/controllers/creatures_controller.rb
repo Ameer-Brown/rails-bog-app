f330b2472a315fd3fe29a956190640482bee054d
Ameer Brown <akeithbrown@gmail.com>
2016-05-02 08:41:29 -0700

    update & delete working

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -10,4 +10,33 @@ class CreaturesController < ApplicationController
   def new
     Creature.new
   end
+
+  def create
+    Creature.create(c_params)
+    redirect_to('/creatures')
+  end
+
+
+  def edit
+    @creature = Creature.find_by_id(params[:id])
+  end
+
+  def destroy
+    @creature =Creature.find(params[:id])
+    @creature.destroy
+    redirect_to('/creatures')
+  end
+
+  def update
+    @creature = Creature.find_by_id(params[:id])
+    @creature.update_attributes(c_params)
+    redirect_to('/creatures')
+  end
+
+ private
+
+ def c_params
+   params.require(:creature).permit(:name, :description)
+ end
+
 end
